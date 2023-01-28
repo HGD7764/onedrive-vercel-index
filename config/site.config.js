f704512d241576e0737f7a943dc0ce2fa11a6e77
HGD7764 <3470896755@qq.com>
2023-01-28 19:52:51 +0800

    upload

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,9 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ["/🔨管理文件/GStarSky",],
+  protectedRoutes: [
+		"/🔨管理文件",
+	],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
